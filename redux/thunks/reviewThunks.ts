bcb3e0e76e6bf394f6e03bd5912f0346c9b69e66
kevin real alejo <kevinrealalejo@mostrasao.home>
2022-03-10 22:25:03 +0100

    create state to get Read of comments

--- redux/thunks/reviewThunks.ts
+++ redux/thunks/reviewThunks.ts
@@ -6,7 +6,6 @@ const url: string = `${process.env.NEXT_PUBLIC_PERFECTREADS_API}/reviews/all`;
 export const loadReviewThunk = async (dispatch: Dispatch) => {
   const response = await fetch(url);
   const reviewList: any = await response.json();
-  console.log(reviewList);
 
   return dispatch(getReviewAction(reviewList));
 };

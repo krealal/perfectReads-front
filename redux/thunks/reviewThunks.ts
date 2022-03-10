7513934775dc148f778b26e530a9d10fc65e9a0e
Kevin Real <kevinreal8994@gmail.com>
2022-03-10 14:27:55 +0100

    create test for loadReviewThunk

--- redux/thunks/reviewThunks.ts
+++ redux/thunks/reviewThunks.ts
@@ -6,7 +6,6 @@ const url: string = `${process.env.NEXT_PUBLIC_PERFECTREADS_API}/reviews/all`;
 
 export const loadReviewThunk = async (dispatch: Dispatch) => {
   const response = await fetch(url);
-  console.log(response);
   const reviewList: Reviews = await response.json();
 
   return dispatch(getReviewAction(reviewList));

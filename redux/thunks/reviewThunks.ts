41f40e95524470b88675296a2307ac57e917409f
kevin real alejo <kevinrealalejo@mostrasao.home>
2022-03-11 19:59:18 +0100

    refactorize to get url from const

--- redux/thunks/reviewThunks.ts
+++ redux/thunks/reviewThunks.ts
@@ -1,22 +1,19 @@
 import { Dispatch } from "redux";
 import { deleteReviewAction, getReviewAction } from "../actions/actionCreator";
 
-const url: string = `${process.env.NEXT_PUBLIC_PERFECTREADS_API}/reviews/all`;
+const url: string = `${process.env.NEXT_PUBLIC_PERFECTREADS_API}`;
 
 export const loadReviewThunk = async (dispatch: Dispatch) => {
-  const response = await fetch(url);
+  const response = await fetch(`${url}/reviews/all`);
   const reviewList: any = await response.json();
 
   return dispatch(getReviewAction(reviewList));
 };
 
 export const deleteReviewThunk = (id: string) => async (dispatch: Dispatch) => {
-  const response = await fetch(
-    `$${process.env.NEXT_PUBLIC_PERFECTREADS_API}/reviews/${id}`,
-    {
-      method: "delete",
-    }
-  );
+  const response = await fetch(`${url}/reviews/${id}`, {
+    method: "DELETE",
+  });
   if (!response.ok) return;
   dispatch(deleteReviewAction(id));
 };

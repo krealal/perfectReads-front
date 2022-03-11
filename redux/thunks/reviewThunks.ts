84b9dcde86797a12c3bc6c18438e9f5fb22aac25
kevin real alejo <kevinrealalejo@mostrasao.home>
2022-03-11 19:05:47 +0100

    create thunk deleteReviewThunk

--- redux/thunks/reviewThunks.ts
+++ redux/thunks/reviewThunks.ts
@@ -1,5 +1,5 @@
 import { Dispatch } from "redux";
-import { getReviewAction } from "../actions/actionCreator";
+import { deleteReviewAction, getReviewAction } from "../actions/actionCreator";
 
 const url: string = `${process.env.NEXT_PUBLIC_PERFECTREADS_API}/reviews/all`;
 
@@ -8,4 +8,15 @@ export const loadReviewThunk = async (dispatch: Dispatch) => {
   const reviewList: any = await response.json();
 
   return dispatch(getReviewAction(reviewList));
+};
+
+export const deleteReviewThunk = (id: string) => async (dispatch: Dispatch) => {
+  const response = await fetch(
+    `$${process.env.NEXT_PUBLIC_PERFECTREADS_API}/reviews/${id}`,
+    {
+      method: "delete",
+    }
+  );
+  if (!response.ok) return;
+  dispatch(deleteReviewAction(id));
 };

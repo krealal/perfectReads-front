5fa0df03b88290f60aa2cc85c65be8e7da8d9dbe
kevin real alejo <kevinrealalejo@mostrasao.home>
2022-03-11 20:00:21 +0100

    add test of deleteReviewThunk with cornercase if not work

--- redux/thunks/reviewThunks.test.ts
+++ redux/thunks/reviewThunks.test.ts
@@ -1,5 +1,5 @@
 import "whatwg-fetch";
-import { loadReviewThunk } from "./reviewThunks";
+import { deleteReviewThunk, loadReviewThunk } from "./reviewThunks";
 import { server } from "../../mocks/server";
 
 // Establish API mocking before all tests.
@@ -20,6 +20,32 @@ describe("Given a loadReviewThunk function", () => {
       await loadReviewThunk(dispatch);
 
       expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a deleteReviewThunk function", () => {
+  describe("When it is called with id 123", () => {
+    test("Then it should dispatch a function", async () => {
+      const dispatch = jest.fn();
+      const id = "123";
+
+      const deleteThunk = deleteReviewThunk(id);
+      await deleteThunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it is called with id 12", () => {
+    test("Then it shouldn't dispatch a function", async () => {
+      const dispatch = jest.fn();
+      const id = "12";
+
+      const deleteThunk = deleteReviewThunk(id);
+      await deleteThunk(dispatch);
+
+      expect(dispatch).not.toBeCalled();
     });
   });
 });

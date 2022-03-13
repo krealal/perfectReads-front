e85f9866abe9324ee97954b79089493df969139e
Kevin Real Alejo <kevinreal8994@gmail.com>
2022-03-13 14:36:03 +0100

    create page for add reviews

---
+++ pages/new-review.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import RegisterForm from "../components/NewReviewForm/NewReviewForm";
+
+const NewReview = (): JSX.Element => {
+  const RegisterCont = styled.section`
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  return (
+    <>
+      <RegisterCont>
+        <RegisterForm />
+      </RegisterCont>
+    </>
+  );
+};
+
+export default NewReview;

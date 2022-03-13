d71d3cbaaa050f3842fafa030d85492584637a87
Kevin Real Alejo <kevinreal8994@gmail.com>
2022-03-13 23:18:10 +0100

    create newReviewForm test

--- components/NewReviewForm/NewReviewForm.test.tsx
+++ components/NewReviewForm/NewReviewForm.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import store from "../../redux/store";
 import RegisterForm from "./NewReviewForm";
 
-describe("", () => {
-  describe("", () => {
-    test("", () => {
+describe("given a registerForm component", () => {
+  describe("when its rendered", () => {
+    test("then it should be find the heading 'new review'", () => {
       render(
         <Provider store={store}>
           <RegisterForm />
@@ -15,6 +16,21 @@ describe("", () => {
       const hola = screen.getByRole("heading", { name: /new review/i });
 
       expect(hola).toBeInTheDocument();
+    });
+  });
+
+  describe("when its rendered", () => {
+    test("then it should be find the heading 'new review'", () => {
+      const inputedText = "hola buenos días";
+
+      render(
+        <Provider store={store}>
+          <RegisterForm />
+        </Provider>
+      );
+
+      const input = screen.getByRole("textbox", { name: /name/i });
+      userEvent.type(input, inputedText);
     });
   });
 });

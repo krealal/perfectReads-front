4e2ada3e62728c0f3317f77b3e79aa3a88fec5cc
Kevin Real <kevinreal8994@gmail.com>
2022-03-15 00:14:16 +0100

    remove unused imports in component form

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { createReviewThunk } from "../../redux/thunks/reviewThunks";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";

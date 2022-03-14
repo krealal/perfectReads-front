664a21f6f60db49501437fcbb7d2369ac2c19f87
Kevin Real Alejo <kevinreal8994@gmail.com>
2022-03-14 21:00:01 +0100

    refactorize form component

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -22,28 +22,16 @@ const TittleForm = styled.h1`
 interface formProps {
   tittle: string;
   buttonText: string;
+  submit: any;
+  changeData: any;
 }
 
-const Form = ({ tittle, buttonText }: formProps): JSX.Element => {
-  const dispatch = useDispatch();
-
-  const blannkFields = {
-    name: "",
-    review: "",
-    score: 1,
-    image: "",
-  };
-  const [formData, setFormData] = useState(blannkFields);
-
-  const changeData = (event: any) => {
-    setFormData({ ...formData, [event.target.id]: event.target.value });
-  };
-
-  const submitForm = (event: any) => {
-    event.preventDefault();
-    dispatch(createReviewThunk(formData));
-  };
-
+const Form = ({
+  tittle,
+  buttonText,
+  submit,
+  changeData,
+}: formProps): JSX.Element => {
   return (
     <>
       <Box
@@ -56,7 +44,7 @@ const Form = ({ tittle, buttonText }: formProps): JSX.Element => {
         }}
         noValidate
         autoComplete="off"
-        onSubmit={submitForm}
+        onSubmit={submit}
       >
         <TittleForm>{tittle}</TittleForm>
         <TextField id="name" label="Name" type="text" onChange={changeData} />
@@ -83,70 +71,6 @@ const Form = ({ tittle, buttonText }: formProps): JSX.Element => {
         </ButtonContainer>
       </Box>
     </>
-
-    // <FormContainer onSubmit={submitForm} autoComplete="off" noValidate>
-    //   <ul>
-    //     <li>
-    //       <h1 className="title">New Review</h1>
-    //       <p className="userReg"></p>
-    //     </li>
-    //     <li>
-    //       <label title="name" htmlFor="name">
-    //         Name
-    //       </label>
-    //       <input
-    //         type="text"
-    //         name="name"
-    //         id="name"
-    //         onChange={changeData}
-    //         required
-    //       />
-    //     </li>
-    //     <li>
-    //       <label title="image" htmlFor="image">
-    //         Image
-    //       </label>
-    //       <input
-    //         type="text"
-    //         name="image"
-    //         id="image"
-    //         onChange={changeData}
-    //         required
-    //       />
-    //     </li>
-    //     <li>
-    //       <label title="score" htmlFor="score">
-    //         Score
-    //       </label>
-    //       <input
-    //         type="number"
-    //         name="score"
-    //         id="score"
-    //         min="1"
-    //         max="5"
-    //         required
-    //       />
-    //     </li>
-    //     <li>
-    //       <label title="review" htmlFor="review">
-    //         Review
-    //       </label>
-    //       <input
-    //         name="review"
-    //         id="review"
-    //         maxLength={105}
-    //         onChange={changeData}
-    //         required
-    //       />
-    //     </li>
-
-    //     <li>
-    //       <button type="submit" className="button">
-    //         Add Review
-    //       </button>
-    //     </li>
-    //   </ul>
-    // </FormContainer>
   );
 };
 

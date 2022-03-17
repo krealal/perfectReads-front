7e909d8ee4e1d8c8a53f4bf2b168b2ed5a96d1bf
Kevin Real <kevinreal8994@gmail.com>
2022-03-17 12:55:47 +0100

    change _app page to be wrapped in the new provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,15 @@ import "../styles/globals.css";
 import "@fontsource/roboto";
 import type { AppProps } from "next/app";
 import Navigation from "../components/layout/Navigation";
-import { Provider } from "react-redux";
-import store from "../redux/store";
+import { FC } from "react";
+import { wrapper } from "../redux/store";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store}>
-      <Navigation>
-        <Component {...pageProps} />
-      </Navigation>
-    </Provider>
+    <Navigation>
+      <Component {...pageProps} />
+    </Navigation>
   );
-}
+};
 
-export default MyApp;
+export default wrapper.withRedux(WrappedApp);

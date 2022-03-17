a857dc1684c87970519c09491654239d8a840bc5
Kevin Real <kevinreal8994@gmail.com>
2022-03-17 14:01:51 +0100

    fix home test with new provider

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,20 +1,17 @@
 import "whatwg-fetch";
 import { render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import Home from "../pages/index";
-import store from "../redux/store";
+import Home, { getServerSideProps } from "../pages/index";
+import { wrapper } from "../redux/store";
+import { GetServerSidePropsContext } from "next";
 
 describe("given a home page ", () => {
   describe("when its rendered", () => {
     test("then it should find h1 /The Way Of Kings/ in the document", async () => {
-      render(
-        <Provider store={store}>
-          <Home />
-        </Provider>
-      );
+      const WrappedComponent = wrapper.withRedux(Home);
+      render(<WrappedComponent />);
       const bookTittle = "The Way Of Kings";
 
-      const renderTittle = await screen.getByRole("heading", {
+      const renderTittle = screen.getByRole("heading", {
         name: bookTittle,
       });
 
@@ -23,26 +20,36 @@ describe("given a home page ", () => {
   });
 
   test("Then it should find marta in the document", async () => {
-    render(
-      <Provider store={store}>
-        <Home />
-      </Provider>
+    const WrappedComponent = await wrapper.withRedux(Home);
+    const context = {
+      params: {},
+    };
+    render(<WrappedComponent />);
+
+    const getSSR = await getServerSideProps(
+      context as GetServerSidePropsContext
     );
 
-    const test = await screen.findByText("marta");
+    const marta = await screen.findByText("marta");
 
-    expect(test).toBeInTheDocument();
+    screen.debug();
+
+    expect(marta).toBeInTheDocument();
   });
 
   test("Then it should find score image with tittle  `luis's score`", async () => {
-    render(
-      <Provider store={store}>
-        <Home />
-      </Provider>
+    const WrappedComponent = wrapper.withRedux(Home);
+    const context = {
+      params: {},
+    };
+    render(<WrappedComponent />);
+
+    const getSSR = await getServerSideProps(
+      context as GetServerSidePropsContext
     );
 
-    const test = await screen.findByTitle(/luis's score/i);
+    const luis = await screen.findByTitle(/luis's score/i);
 
-    expect(test).toBeInTheDocument();
+    expect(luis).toBeInTheDocument();
   });
 });

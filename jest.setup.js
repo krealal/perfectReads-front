73be8e085a4eece313cff17d4473e875efaff9b9
Kevin Real <kevinreal8994@gmail.com>
2022-03-17 15:55:59 +0100

    create custom store for wrappers

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,24 @@
 import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import "whatwg-fetch";
 import { server } from "./mocks/server";
+import rootReducer from "./redux/reducers";
 
 beforeAll(() => server.listen());
 
 afterEach(() => server.resetHandlers());
 
 afterAll(() => server.close());
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export const renderWithProviders = (component) => {
+  const Providers = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return render(component, { wrapper: Providers });
+};

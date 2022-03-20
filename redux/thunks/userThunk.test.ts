2d606fa79bcfde2e2f661efc3bfd7a842698de01
Kevin Real Alejo <kevinreal8994@gmail.com>
2022-03-20 01:23:02 +0100

    test login thunk

---
+++ redux/thunks/userThunk.test.ts
@@ -0,0 +1,30 @@
+import "whatwg-fetch";
+import { server } from "../../mocks/server";
+import { loginThunk } from "./userThunk";
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished.
+afterAll(() => server.close());
+
+describe("Given a loginThunk function", () => {
+  describe("When it is called", () => {
+    test("Then it should dispatch a function", async () => {
+      const dispatch = jest.fn();
+      const user = {
+        username: "luis",
+        password: "1234",
+      };
+      const login = loginThunk(user);
+
+      await login(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
--- redux/thunks/userThunk.ts
+++ redux/thunks/userThunk.ts
@@ -1,11 +1,10 @@
-import jwtDecode from "jwt-decode";
 import { Dispatch } from "redux";
 import { loginAction } from "../actions/actionCreator";
 import Router from "next/router";
 
 const url: string = `${process.env.NEXT_PUBLIC_PERFECTREADS_API}`;
 
-export const userThunk = (user: any) => async (dispatch: Dispatch) => {
+export const loginThunk = (user: any) => async (dispatch: Dispatch) => {
   const response = await fetch(`${url}/user/login`, {
     method: "POST",
     headers: {
@@ -15,9 +14,12 @@ export const userThunk = (user: any) => async (dispatch: Dispatch) => {
   });
 
   if (!response.ok) return;
+
   const token = await response.json();
-  const { username }: any = await jwtDecode(token.token);
+  const { username }: any = response;
   localStorage.setItem("token", token.token);
   dispatch(loginAction({ username, password: token.token }));
-  Router.push("/");
+  () => {
+    Router.push("/");
+  };
 };

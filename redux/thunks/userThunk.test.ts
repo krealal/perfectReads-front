5fe8a1eb47154491823da6d4e1e443c408202e73
Kevin Real <kevinreal8994@gmail.com>
2022-03-21 11:59:23 +0100

    create test for registerThunk

--- redux/thunks/userThunk.test.ts
+++ redux/thunks/userThunk.test.ts
@@ -1,6 +1,6 @@
 import "whatwg-fetch";
 import { server } from "../../mocks/server";
-import { loginThunk } from "./userThunk";
+import { loginThunk, registerThunk } from "./userThunk";
 
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
@@ -23,6 +23,27 @@ describe("Given a loginThunk function", () => {
         password: "1234",
       };
       const login = loginThunk(user);
+
+      await login(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a register thunk", () => {
+  describe("When it is called", () => {
+    test("Then it should dispatch a function", async () => {
+      const dispatch = jest.fn();
+      const user = {
+        name: "iwi",
+        username: "uwu",
+        password: "1234",
+        about: "i like turtles",
+        image: "src/1.jpg",
+        email: "uwu@uwu.uwu",
+      };
+      const login = registerThunk(user);
 
       await login(dispatch);
 

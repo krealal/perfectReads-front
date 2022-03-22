da7036994f2af0bf5ab8f8ffaad9322b7b87a728
Kevin Real <kevinreal8994@gmail.com>
2022-03-22 22:42:54 +0100

    delete unused imports in newreview

--- pages/new-review.tsx
+++ pages/new-review.tsx
@@ -1,11 +1,10 @@
 import Router from "next/router";
 import React, { FormEventHandler, useState } from "react";
 import { useDispatch } from "react-redux";
-import { toast, ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import styled from "styled-components";
 import Form from "../components/Form/Form";
 import { createReviewThunk } from "../redux/thunks/reviewThunks";
-import toastMessage from "../utils/toast";
 
 const RegisterCont = styled.section`
   height: 100vh;
